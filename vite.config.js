cb191cd600839a4a5a85100f88eb2cf270d2d0ad
Paul Ross <pross326@gmail.com>
2024-07-04 22:41:34 -0400

    remove path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "pross326.github.io/",
+  base: "/pross326.github.io/",
   plugins: [vue()],
 });
