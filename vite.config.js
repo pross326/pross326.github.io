c9395cf5790c0fe9058cf15f20454e21336d2200
Paul Ross <pross326@gmail.com>
2024-07-04 23:15:10 -0400

    add line to index file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: "/pross326.github.io/",
   plugins: [vue()],
