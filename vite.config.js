a47601ab02d362470893c094ce3c9fe2dc065341
Paul Ross <pross326@gmail.com>
2024-07-05 11:31:28 -0400

    add base path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  // base: '/pross326.github.io/',
+  base: "/pross326.github.io/",
   plugins: [vue()],
 });
